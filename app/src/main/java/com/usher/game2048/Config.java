b8b968019046411268e8fca2a759755c7d1f1999
legolas007 <oukuanou@163.com>
2018-03-08 15:11:00 +0800

    v1.0
    Version1.0

---
+++ app/src/main/java/com/usher/game2048/Config.java
@@ -0,0 +1,10 @@
+package com.usher.game2048;
+
+/**
+ * Created by 24219 on 2018/3/7/0007.
+ */
+
+public class Config {
+    public static final int LINES = 4;
+    public static int CARD_WIDTH = 0;
+}
